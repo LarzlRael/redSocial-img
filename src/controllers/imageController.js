dee29a773ba547dc937d435799c6686b091efd75
Rey <gato_xD@gmail.com>
2019-07-18 16:10:01 -0400

    registra en la base de datos de mongo

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -2,22 +2,39 @@ const imageController = {};
 const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fsextra = require('fs-extra');
+// importando el moodelo
+const { Image } = require('../models/index');
 imageController.index = (req, res) => {
     res.render('index');
 }
 imageController.create = async (req, res) => {
 
+
     const imgURL = randomNumber();
-    console.log(imgURL);
+
 
     const ext = path.extname(req.file.originalname).toLowerCase();
     const image_path = req.file.path;
     const tartGetPath = path.resolve(`src/public/upload/${imgURL}${ext}`);
     console.log(tartGetPath);
 
-    res.send('subido :D')
+
     if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
         await fsextra.rename(image_path, tartGetPath);
+
+        const newImage = new Image({
+            title:req.body.title,
+            fileName:req.body.description+ext,
+            description:req.body.description
+        })
+
+        const imageSave = await newImage.save();
+        console.log('imagen guarada :D ');
+        res.send('guarada')
+        
+    } else {
+        await fsextra.unlink(image_path);
+        res.status(500).json({ error: 'only image are allowed' })
     }
 
 }
